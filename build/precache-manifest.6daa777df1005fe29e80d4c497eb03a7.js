d3128b75df3d868cb2fb51278b6355aecb2bbafe
Jill Moore <okjill@gmail.com>
2020-06-01 00:35:49 -0500

    run deploy

---
+++ build/precache-manifest.6daa777df1005fe29e80d4c497eb03a7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9942b91ddeba7ab193a40a9e1ef32eac",
+    "url": "/okjill/index.html"
+  },
+  {
+    "revision": "d88bc7a6cd83f4e02ca3",
+    "url": "/okjill/static/css/main.c7260cc0.chunk.css"
+  },
+  {
+    "revision": "7cdd29f2fc9353c207fc",
+    "url": "/okjill/static/js/2.17e89252.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/okjill/static/js/2.17e89252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d88bc7a6cd83f4e02ca3",
+    "url": "/okjill/static/js/main.f5233e74.chunk.js"
+  },
+  {
+    "revision": "3a0a1c25b168d9b2b8eb",
+    "url": "/okjill/static/js/runtime-main.1dc84f77.js"
+  },
+  {
+    "revision": "fed1d73519a77aa04fc8ef6b80243517",
+    "url": "/okjill/static/media/GlamourAbsolute_Condensed.fed1d735.otf"
+  },
+  {
+    "revision": "6e1218e1caa9d77d355f87d0c4d6e747",
+    "url": "/okjill/static/media/GlamourAbsolute_Extended.6e1218e1.otf"
+  },
+  {
+    "revision": "08824461412a203e11b6949193dadf3e",
+    "url": "/okjill/static/media/GlamourAbsolute_Regular.08824461.otf"
+  },
+  {
+    "revision": "a1ee4a9aadb9c7db3bef3f8a38af7f71",
+    "url": "/okjill/static/media/jill.a1ee4a9a.png"
+  },
+  {
+    "revision": "ae6a6902ec0564d6dff8e08fa27b858a",
+    "url": "/okjill/static/media/logo.ae6a6902.svg"
+  },
+  {
+    "revision": "18c0a49030de69aff684f2b025a2e913",
+    "url": "/okjill/static/media/resume.18c0a490.png"
+  },
+  {
+    "revision": "df58e11bdc23c21e7aa921920f5035b0",
+    "url": "/okjill/static/media/resume.df58e11b.pdf"
+  }
+]);
